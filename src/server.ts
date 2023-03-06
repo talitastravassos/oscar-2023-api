3c9c7813129fe4e0649590a19b787254b3360ec5
talitastravassos <talitatravassos.tt@gmail.com>
2023-03-06 16:17:48 -0300

    add cors

--- src/server.ts
+++ src/server.ts
@@ -10,6 +10,16 @@ dotenv.config();
 
 const port = 3333;
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+  );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 app.get('/', getCategoriesController);
 
 // routes
